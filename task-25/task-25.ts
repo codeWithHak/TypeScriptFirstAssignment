7340e6689ee7baeb4e742c9946030655b77a2f58
Huzair Ahmed Khan <huzairahmedkhan@gmail.com>
2024-02-23 22:25:26 +0500

    Completed 24 tasks

---
+++ task-25/task-25.ts
@@ -0,0 +1,11 @@
+let alien_color = "red"
+if (alien_color == "green") {
+    console.log("Congrats! you just earned 5 points");
+    
+}
+
+let alien_color_2 = "green"
+ if (alien_color_2 == "green") {
+    console.log("Congrats! you just earned 5 points");
+    
+ }
