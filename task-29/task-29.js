ed1950da5ae8f3a835d03dd2f52ceca8f1b60637
Uzair Ahmed Khan / HO / Zensoft <uzair.khan@ho.gfg.com.pk>
2024-02-24 14:13:22 +0500

    completed task-29

--- task-29/task-29.js
+++ task-29/task-29.js
@@ -1,12 +1,32 @@
 "use strict";
+// includes method is not working i googled it says i have to make changes in my tsconfig.json in lib or change version to es6 or above but its still dosent work so i used other syntax
 Object.defineProperty(exports, "__esModule", { value: true });
-var fruits = ["apple", "banana", "mango", "pineapple", "wtermelon"];
-if (fruits.includes("apple")) {
+var fruits = ["apple", "banana", "mango", "pineapple"];
+if (fruits.indexOf("apple") !== -1) {
     console.log("Apple is one of my favurite fruits");
 }
-if (fruits.includes("banana")) {
+if (fruits.indexOf("banana") !== -1) {
     console.log("Banana is one of my favurite fruits");
 }
-if (fruits.includes("mango")) {
+if (fruits.indexOf("mango") !== -1) {
     console.log("Mango is one of my favurite fruits");
+}
+if (fruits.indexOf("pineapple") !== -1) {
+    console.log("Pineapple is one of my favurite fruits");
+}
+var fav_fruits = ["grapes", "pomegranate", "orange"];
+if (fav_fruits.indexOf("grapes") !== -1) {
+    console.log("Yoy really like ".concat(fav_fruits[0]));
+}
+if (fav_fruits.indexOf("bananas") !== -1) {
+    console.log("Yoy really like ".concat(fav_fruits[1]));
+}
+if (fav_fruits.indexOf("apple") !== -1) {
+    console.log("Yoy really like ".concat(fav_fruits[1]));
+}
+if (fav_fruits.indexOf("pomegranate") !== -1) {
+    console.log("Yoy really like ".concat(fav_fruits[1]));
+}
+if (fav_fruits.indexOf("orange") !== -1) {
+    console.log("Yoy really like ".concat(fav_fruits[2]));
 }
--- task-29/task-29.ts
+++ task-29/task-29.ts
@@ -1,20 +1,42 @@
+// includes method is not working i googled it says i have to make changes in my tsconfig.json in lib or change version to es6 or above but its still dosent work so i used other syntax
+
 let fruits = ["apple","banana","mango","pineapple"]
-if (fruits.includes("apple")){
+if (fruits.indexOf("apple")!==-1){
     console.log("Apple is one of my favurite fruits");
     
 }
 
-if (fruits.includes("banana")) {
+if (fruits.indexOf("banana")!==-1) {
     console.log("Banana is one of my favurite fruits")
 }
 
-if (fruits.includes("mango")) {
+if (fruits.indexOf("mango") !== -1) {
     console.log("Mango is one of my favurite fruits")
 }
 
-if (fruits.includes("pineapple")) {
+if (fruits.indexOf("pineapple")!==-1) {
     console.log("Pineapple is one of my favurite fruits")
 }
 
-let fav_fruits = ["gava", "pomegrnate","tomato" ]
+let fav_fruits = ["grapes", "pomegranate","orange" ];
+
+if (fav_fruits.indexOf("grapes")!==-1){
+    console.log(`Yoy really like ${fav_fruits[0]}`);  
+}
+
+if (fav_fruits.indexOf("bananas")!==-1){
+    console.log(`Yoy really like ${fav_fruits[1]}`);  
+}
+
+if (fav_fruits.indexOf("apple")!==-1){
+    console.log(`Yoy really like ${fav_fruits[1]}`);  
+}
+
+if (fav_fruits.indexOf("pomegranate")!==-1){
+    console.log(`Yoy really like ${fav_fruits[1]}`);  
+}
+if (fav_fruits.indexOf("orange")!==-1){
+    console.log(`Yoy really like ${fav_fruits[2]}`);  
+}
+
 export{}
